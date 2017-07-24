e30daa24a64610c7ed1c0a070d9e24b524f9871d
tuannt <tuanbaros@yahoo.com.vn>
2017-07-24 11:20:09 +0700

    init travis

--- app/src/main/java/com/simple/checkstyledemo/MainActivity.java
+++ app/src/main/java/com/simple/checkstyledemo/MainActivity.java
@@ -1,13 +1,23 @@
 package com.simple.checkstyledemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.app.Application;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int s;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int i = 3;
+        test(i);
+    }
+
+    private void test(int a) {
+        if (a > 3) return;
+        System.out.println(a);
     }
 }
