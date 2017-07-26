1c1abd7ed8e7b66c27e6d81d6cf67dbe637d6a09
tuannt <tuanbaros@yahoo.com.vn>
2017-07-26 15:57:25 +0700

    comment github pr with circleci

--- app/src/main/java/com/simple/checkstyledemo/MainActivity.java
+++ app/src/main/java/com/simple/checkstyledemo/MainActivity.java
@@ -1,8 +1,10 @@
 package com.simple.checkstyledemo;
 
 import android.os.Bundle;
+import android.support.annotation.Keep;
 import android.support.v7.app.AppCompatActivity;
 
+@Keep
 public class MainActivity extends AppCompatActivity {
 
     private int s;
@@ -16,7 +18,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void test(int a) {
-        if (a > 3)return;
+        if (a > 3){
+            return;
+        }
         s = a;
     }
 }
